5a5bb2df0bdb4cd17671b3c0e1500420d79b6b55
Andrew Chan <andrewchan6717@gmail.com>
2026-07-16 12:46:03 -0600

    Fix GitHub Pages path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  site: 'https://pandatheman.github.io',
+  base: '/ryanfitztemplate/',
 });
