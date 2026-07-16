c93bd303a58014224b3ecd88740797b8320f684f
Andrew Chan <andrewchan6717@gmail.com>
2026-07-16 13:00:35 -0600

    Fix Astro base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://pandatheman.github.io',
-  base: '/ryanfitztemplate',
+  base: '/ryanfitztemplate/',
 });
